b974ece3a91067ebec59a1e511c080254c410f24
nguyen duc hau <hau.nguyenbk8786@gmail.com>
2022-12-08 23:35:58 +0700

    CRUD product

--- admin/login.php
+++ admin/login.php
@@ -84,7 +84,7 @@ if (isset($_POST['login_ad'])) {
 </div>
 
 <?php
-    require '../includes/footer.php';
+    require './includes/footer.php';
 ?>
 <!-- JavaScript Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
